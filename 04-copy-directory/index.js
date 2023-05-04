7fb0bac04911422b984e67dbcc79b74bd9e77ebb
Tupalski Ilya <itupalski@gmail.com>
2023-05-04 22:54:45 +0400

    feat: add task5 merge-style (Thu May  4 22:54:45 +04 2023)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,13 +2,16 @@ const fs = require("fs");
 const path = require("path");
 const { COPYFILE_EXCL } = fs.constants;
 
-fs.mkdir(path.join(__dirname, "files-copy"), {recursive: true}, () => {
-    fs.readdir(path.join(__dirname, "files"), {withFileTypes: true}, (err, files) => {
-
-        files.forEach((file) => {
-
-            fs.copyFile(path.join(__dirname, "files", `${file.name}`), path.join(__dirname, "files-copy", `${file.name}`), COPYFILE_EXCL, () => {})
+function copyDir () {
+    fs.mkdir(path.join(__dirname, "files-copy"), {recursive: true}, () => {
+        fs.readdir(path.join(__dirname, "files"), {withFileTypes: true}, (err, files) => {
+    
+            files.forEach((file) => {
+    
+                fs.copyFile(path.join(__dirname, "files", `${file.name}`), path.join(__dirname, "files-copy", `${file.name}`), COPYFILE_EXCL, () => {})
+            })
+            console.log("files copied");
         })
-        console.log("files copied");
     })
-})
+}
+copyDir();
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const addFileCss = fs.WriteStream(path.join(__dirname, "project-dist" , "bundle.css"));
+
+function readAndWriteFile (fileName) {
+    const readingFile = new fs.ReadStream(path.join(__dirname, "styles" , `${fileName}`), {encoding: "utf8"});
+ 
+    readingFile.on('readable', function(){
+        const text = readingFile.read();
+        if(text != null)addFileCss.write(text);
+    });
+}
+
+fs.readdir(path.join(__dirname, "styles"), {withFileTypes: true}, (err, files) => {
+
+    files.forEach((file) => {
+        if (path.extname(file.name) === ".css" && file.isFile()) {
+            readAndWriteFile(file.name);
+        }
+    })
+})
