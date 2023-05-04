f68d5128c6e72902558a9243bb35a4e626fb9f59
Tupalski Ilya <itupalski@gmail.com>
2023-05-04 22:17:17 +0400

    feat: add task 4 copy file (Thu May  4 22:17:17 +04 2023)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const { COPYFILE_EXCL } = fs.constants;
 
+fs.mkdir(path.join(__dirname, "files-copy"), {recursive: true}, () => {
+    fs.readdir(path.join(__dirname, "files"), {withFileTypes: true}, (err, files) => {
+
+        files.forEach((file) => {
+
+            fs.copyFile(path.join(__dirname, "files", `${file.name}`), path.join(__dirname, "files-copy", `${file.name}`), COPYFILE_EXCL, () => {
+                console.log('complite')
+            })
+        })
+    })
+})
