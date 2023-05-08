e4be71ae6f393d2a6ecea63d9bda8a4e8ca74f1c
Tupalski Ilya <itupalski@gmail.com>
2023-05-08 12:39:04 +0400

    fix: task4 delete directory before copy (Mon May  8 12:39:04 +04 2023)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,14 +3,16 @@ const path = require("path");
 const { COPYFILE_EXCL } = fs.constants;
 
 function copyDir () {
-    fs.mkdir(path.join(__dirname, "files-copy"), {recursive: true}, () => {
-        fs.readdir(path.join(__dirname, "files"), {withFileTypes: true}, (err, files) => {
-    
-            files.forEach((file) => {
-    
-                fs.copyFile(path.join(__dirname, "files", `${file.name}`), path.join(__dirname, "files-copy", `${file.name}`), COPYFILE_EXCL, () => {})
+    fs.rm(path.join(__dirname, "files-copy"), {recursive: true, force: true}, (err) => {
+        fs.mkdir(path.join(__dirname, "files-copy"), {recursive: true}, () => {
+            fs.readdir(path.join(__dirname, "files"), {withFileTypes: true}, (err, files) => {
+        
+                files.forEach((file) => {
+        
+                    fs.copyFile(path.join(__dirname, "files", `${file.name}`), path.join(__dirname, "files-copy", `${file.name}`), COPYFILE_EXCL, () => {})
+                })
+                console.log("files copied");
             })
-            console.log("files copied");
         })
     })
 }
